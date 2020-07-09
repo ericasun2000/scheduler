8ee756d7db780ae23e0be73efa0c42812375a669
Erica Sun <ericasun@seas.upenn.edu>
2020-07-09 15:59:45 -0400

    Implement and test back function in useVisualMode hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,10 +2,19 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   function transition(mode) {
     setMode(mode);
+    setHistory([...history, mode]);
   }
 
-  return { mode, transition }
+  function back() {
+    if (history.length > 1) {
+      setMode(history[history.length - 2])
+      setHistory(history.slice(0, -1));
+    }
+  }
+
+  return { mode, transition, back }
 }
