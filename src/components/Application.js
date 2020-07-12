8c80184f10ac8427448329800b0ad6f7a211e623
Erica Sun <ericasun@seas.upenn.edu>
2020-07-11 20:13:27 -0400

    Separate rendering from state management using useApplicationData custom hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,70 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
-import axios from "axios"; 
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "../helpers/selectors.js"
+import useApplicationData from "../hooks/useApplicationData.js"
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-
-  const setDay = day => { setState({...state, day}) }; // curly bc dont need it to be returned. want effects of setState. Return keyword without curly is overkill (bascially two return words, should be error)
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview }
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.put(`api/appointments/${id}`, appointment)
-      .then(() => setState({...state, appointments}))
-      .catch((error) => { return Promise.reject(error)})
-  }
-
-  // return (       axios.delete(`/api/appointments/${id}`)         .then((response) => {           setState({...state, appointments})         })         .catch((error) => {           return Promise.reject(error);         })     )
-
-  function cancelInterview(id) {
-    const appointment = {
-        ...state.appointments[id],
-        interview: null
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.delete(`api/appointments/${id}`)
-      .then(() => setState({...state, appointments}))
-      .catch(error => { return Promise.reject(error)})
-  }
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
-    ])
-    .then(all => { // with curly braces, setState is not being returned. But its not supposed to be returned. 
-      setState({
-        days: all[0].data, 
-        appointments: all[1].data,
-        interviewers: all[2].data
-      })
-    })
-  }, []);
-
+  const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
+  
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
-
 
   const appointmentElements = interviewers && appointments.map(appointment => {
     const interview = getInterview(state, appointment.interview);
---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import axios from "axios"; 
+
+export default function useApplicationData() {
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  });
+  const setDay = day => { setState({...state, day}) }; // curly bc dont need it to be returned. want effects of setState. Return keyword without curly is overkill (bascially two return words, should be error)
+
+  function bookInterview(id, interview) {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview }
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.put(`api/appointments/${id}`, appointment)
+      .then(() => setState({...state, appointments}))
+      .catch((error) => { return Promise.reject(error)})
+  }
+
+  function cancelInterview(id) {
+    const appointment = {
+        ...state.appointments[id],
+        interview: null
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment
+    };
+    return axios.delete(`api/appointments/${id}`)
+      .then(() => setState({...state, appointments}))
+      .catch(error => { return Promise.reject(error)})
+  }
+
+  useEffect(() => {
+    Promise.all([
+      axios.get("http://localhost:8001/api/days"),
+      axios.get("http://localhost:8001/api/appointments"),
+      axios.get("http://localhost:8001/api/interviewers")
+    ])
+    .then(all => { // with curly braces, setState is not being returned. But its not supposed to be returned. 
+      setState({
+        days: all[0].data, 
+        appointments: all[1].data,
+        interviewers: all[2].data
+      })
+    })
+  }, []);
+  
+  return { state: state, setDay, bookInterview, cancelInterview }
+}
+
