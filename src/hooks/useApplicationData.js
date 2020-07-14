373be81dd91b9a0bda4ff0c7194f5f2f849f63f6
Erica Sun <ericasun@seas.upenn.edu>
2020-07-14 15:18:45 -0400

    Test click displays the correct appointments for day using mock axios module

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -52,9 +52,9 @@ export default function useApplicationData() {
 
   useEffect(() => {
     Promise.all([
-      axios.get("http://localhost:8001/api/days"),
-      axios.get("http://localhost:8001/api/appointments"),
-      axios.get("http://localhost:8001/api/interviewers")
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ])
     .then(all => { // with curly braces, setState is not being returned. But its not supposed to be returned. 
       setState({
