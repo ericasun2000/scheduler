f5421f49f4389b0ae37e52760bf87475e755d404
Erica Sun <ericasun@seas.upenn.edu>
2020-07-16 14:42:28 -0400

    Write edit interview cypress test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -25,4 +25,23 @@ describe("Appointments", () => {
     cy.contains(".appointment__card--show", "Lydia Miller-Jones")
       .contains(".appointment__card--show", "Sylvia Palmer");
   });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]")
+      .first()
+      .click({ force: true });
+
+    cy.get("[data-testid=student-name-input]")
+    .clear()
+    .type("Lydia")
+    .get("[alt='Tori Malcolm']")
+    .click();
+
+  cy.contains("Save")
+    .click();
+
+  cy.contains(".appointment__card--show", "Lydia")
+    .contains(".appointment__card--show", "Tori Malcolm");
+
+  })
 });
